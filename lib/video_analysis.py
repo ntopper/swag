88bfa99806bfe740c2769c56b355cb4255c053df
Rodney N <doomrodney@gmail.com>
2015-01-20 15:43:49 -0500

    added resize to video analysis

--- lib/video_analysis.py
+++ lib/video_analysis.py
@@ -1,5 +1,20 @@
-import trial_video
+import cv2
+
+def ImageResize(frame):
+    #maintaing aspec ratio... keeps image from distorting
+
+    r = 1000.0 / frame.shape[1]
+    dim = (1000, int(frame.shape[0]*r))
+
+    #resizing of the image
+    resized = cv2.resize(frame,dim,interpolation = cv2.INTER_AREA)
+
+
+
+         
 
 vid = "GOPR0167.MP4"
 
 video = trial_video.debug(vid)
+
+
